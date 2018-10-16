75d7cc12ef00d2622fe9cc0335f6c023ce5411fb
afmckeown <afmckeown@gmail.com>
2018-10-16 12:55:39 -0700

    Setup for heroku

--- Server/db/mongoose.js
+++ Server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 // Use the default es6 promise rather than 3rd party
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
--- Server/server.js
+++ Server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -37,13 +38,13 @@ app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if(!ObjectID.isValid(id)) {
-    res.status(407).send();
+    res.status(404).send();
     console.log("fuck")
     return;
   }
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      res.status(405).send();
+      res.status(404).send();
       console.log("fuck me")
       return;
     }
@@ -53,8 +54,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 
